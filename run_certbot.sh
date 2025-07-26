e476aec3bae70eb5d95cf0c10cbcf947cfa49d13
server-tools <168527710+server-tools@users.noreply.github.com>
2025-07-26 09:35:34 +0200

    Create run_certbot.sh

---
+++ run_certbot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <email> <domain>"
+    exit 1
+fi
+
+# Assign command-line arguments to variables
+EMAIL="$1"
+DOMAIN="$2"
+
+# Run certbot command as zextras user
+sudo -u zextras /opt/zextras/libexec/certbot certonly \
+    --preferred-chain "ISRG Root X1" \
+    --agree-tos \
+    --email "$EMAIL" \
+    -n \
+    --keep \
+    --webroot \
+    -w /opt/zextras \
+    --cert-name "$DOMAIN" \
+    -d "$DOMAIN"

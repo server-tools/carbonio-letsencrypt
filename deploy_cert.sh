4b97d6de55f50bbda0ed7485fa04f8ea187bb25a
server-tools <168527710+server-tools@users.noreply.github.com>
2025-07-26 10:04:40 +0200

    Update deploy_cert.sh

--- deploy_cert.sh
+++ deploy_cert.sh
@@ -10,6 +10,20 @@ fi
 # Set domain name variable
 DOMAIN_NAME="$1"
 
+DOMAIN_NAME="$1"
+BASE_PATH="/opt/zextras/common/certbot/etc/letsencrypt/live"
+TARGET_DIR="${BASE_PATH}/${DOMAIN_NAME}"
+CERT_DIR_PATTERN="${BASE_PATH}/${DOMAIN_NAME}*"
+
+# Find the latest certificate directory
+LATEST_DIR=$(ls -d ${CERT_DIR_PATTERN} 2>/dev/null | sort -V | tail -n 1)
+
+if [ -z "$LATEST_DIR" ]; then
+  echo "Error: No certificate directories found for intranet-${DOMAIN_NAME}"
+  exit 1
+fi
+
+
 # Execute commands as zextras user
 su - zextras -c "
 # Download ISRG Root X1 certificate
@@ -17,18 +31,18 @@ wget https://letsencrypt.org/certs/isrgrootx1.pem -O /opt/zextras/ssl/carbonio/c
 
 rm /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
 # Create zextras_ca.pem by combining fullchain and isrgrootx1
-cat /opt/zextras/common/certbot/etc/letsencrypt/live/${DOMAIN_NAME}/chain.pem /opt/zextras/ssl/carbonio/commercial/isrgrootx1.pem > /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
+cat ${LATEST_DIR}/chain.pem /opt/zextras/ssl/carbonio/commercial/isrgrootx1.pem > /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
 
 # Verify certificate
-/opt/zextras/bin/zmcertmgr verifycrt comm /opt/zextras/common/certbot/etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem /opt/zextras/common/certbot/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
+/opt/zextras/bin/zmcertmgr verifycrt comm ${LATEST_DIR}/privkey.pem ${LATEST_DIR}/fullchain.pem /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
 
 
 # Copy private key
 rm /opt/zextras/ssl/carbonio/commercial/commercial.key
-cp /opt/zextras/common/certbot/etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem /opt/zextras/ssl/carbonio/commercial/commercial.key
+cp ${LATEST_DIR}/privkey.pem /opt/zextras/ssl/carbonio/commercial/commercial.key
 
 # Deploy certificate
-/opt/zextras/bin/zmcertmgr deploycrt comm /opt/zextras/common/certbot/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
+/opt/zextras/bin/zmcertmgr deploycrt comm ${LATEST_DIR}/fullchain.pem /opt/zextras/ssl/carbonio/commercial/zextras_ca.pem
 
 # Restart Zimbra services
 #zmcontrol restart
